9ca3070682f47cf065e38cac82003d7d5c9686a7
kjuszkiewicz95 <kjuszkiewicz95@gmail.com>
2016-03-09 15:49:07 -0500

    Modified fragment_course_category_passed layout to include an actual listview instead of just dummy layout includes.

--- app/src/main/java/com/example/konrad/coursehub/CourseGradeCategoryPagePassed.java
+++ app/src/main/java/com/example/konrad/coursehub/CourseGradeCategoryPagePassed.java
@@ -47,7 +47,7 @@ public class CourseGradeCategoryPagePassed extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v =  inflater.inflate(R.layout.fragment_course_category_passed, container, false);
+        View v =  inflater.inflate(R.layout.fragment_course_category_passed2, container, false);
         mUpcomingButton = (Button)v.findViewById(R.id.upcomingButton);
         mUpcomingButton.setOnClickListener(new View.OnClickListener() {
             @Override
